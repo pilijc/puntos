12459848881446e4f6aba24aa970e8cb6171556b
kayshamir <besin.ks@sprobe.com>
2026-03-02 16:33:14 +0800

    User Location Bug (#13)

--- src/app/(user)/index.tsx
+++ src/app/(user)/index.tsx
@@ -21,6 +21,7 @@ Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN);
 export default function Discover() {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const cameraRef = useRef(null);
+  const [mapReady, setMapReady] = useState(false);
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
@@ -189,17 +190,25 @@ export default function Discover() {
     (async () => {
       const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") return;
-
-      const loc = await Location.getCurrentPositionAsync({});
-      setLocation(loc);
-
-      cameraRef.current?.setCamera({
-        centerCoordinate: [loc.coords.longitude, loc.coords.latitude],
-        zoomLevel: 14,
-        animationDuration: 1000,
+  
+      const loc = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
       });
+  
+      setLocation(loc);
     })();
   }, []);
+
+  useEffect(() => {
+    if (!mapReady || !location) return;
+    const { longitude, latitude } = location.coords;
+  
+    cameraRef.current?.setCamera({
+      centerCoordinate: [longitude, latitude],
+      zoomLevel: 14,
+      animationDuration: 1000,
+    });
+  }, [mapReady, location]);
 
   console.log(location);
 
@@ -255,6 +264,7 @@ export default function Discover() {
 
       <MapView
         style={{ flex: 1 }}
+        onDidFinishLoadingMap={() => setMapReady(true)}
         styleURL={isDark ? "mapbox://styles/mapbox/navigation-night-v1" : "mapbox://styles/mapbox/streets-v12"}
       >
         <Mapbox.Camera
