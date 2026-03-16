5ba2ed5c181d0a9439697c392d021cc909f14def
Junriel Casul <casul.j@sprobe.com>
2026-03-16 16:43:59 +0800

    Minimal qr changes FD

--- src/app/(front_desk)/index.tsx
+++ src/app/(front_desk)/index.tsx
@@ -124,8 +124,6 @@ export default function FrontDeskScan() {
             },
           ],
         });
-        // setErrorMessage(result.message);
-        // setShowErrorModal(true);
       }
     } catch (error) {
       console.error("Scan error:", error);
@@ -151,7 +149,7 @@ export default function FrontDeskScan() {
             },
           ],
         });
-    //  setShowErrorModal(true);
+     
     }
   };
 
@@ -168,15 +166,15 @@ export default function FrontDeskScan() {
   };
 
   const handleModalClose = () => {
-    // Add this scan to recent scans before resetting
+    
     const amount = parseFloat(purchaseAmount);
     setRecentScans(prev => [{
       points: successPoints,
       timestamp: new Date(),
       amount: amount
-    }, ...prev.slice(0, 4)]); // Keep only last 5 scans
+    }, ...prev.slice(0, 4)]); 
     
-    // Reset scan state
+     
     setScanned(false);
     setIsProcessing(false);
     setShowCamera(false);
