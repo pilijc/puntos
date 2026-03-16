d0eb0cda7b516ea9edd9d5782b9595f3772a318e
Junriel Casul <casul.j@sprobe.com>
2026-03-16 08:21:12 +0800

    refactor layout at user side qr tab

--- src/app/(user)/_layout.tsx
+++ src/app/(user)/_layout.tsx
@@ -1,14 +1,16 @@
 import { Tabs } from 'expo-router';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, useColorScheme, Platform, TouchableOpacity } from 'react-native';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { CustomTabBarButton } from '@/components/qr/qr-button';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
   const insets = useSafeAreaInsets();
+  
 
   return (
     <Tabs
@@ -69,42 +71,4 @@ export default function TabLayout() {
     </Tabs>
   );
 }
-
-function CustomTabBarButton({ onPress, bottomInset }: { onPress?: () => void, bottomInset: number }) {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      activeOpacity={0.8}
-      style={[
-        styles.fabContainer, 
-        { top: Platform.OS === 'ios' ? -30 : -28 - (bottomInset / 4) }
-      ]}
-    >
-      <View style={styles.fab}>
-        <MaterialCommunityIcons name="qrcode-scan" size={28} color="#FFFFFF" />
-      </View>
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  fabContainer: {
-    width: 70,
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fab: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: '#FF6600',
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 6,
-    elevation: 6,
-  },
-});
+  
