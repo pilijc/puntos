27bc430a0bd634b3c20a6f9332567248b2503a33
Junriel Casul <casul.j@sprobe.com>
2026-03-30 09:32:33 +0800

    Bug fixes

--- src/services/users/qr-service.ts
+++ src/services/users/qr-service.ts
@@ -155,7 +155,6 @@ export async function createQRTransaction(
   }
 
   // Decrease the stored_amount in points table
-   
 
   // Award a stamp if the store has the program enabled (do this BEFORE QR transaction so real-time listeners fetching stamps get the latest data)
   await addStamp(userId, storeId);
