b7132a3da5efc77bb293a39e017a4bee1e1d7698
kayshamir <besin.ks@sprobe.com>
2026-03-31 14:02:21 +0800

    [174] - Stamp Manager Stamp Refactor: Draft Edit + Infinite Reward Picker + Delete Stamp (#128)
    * header reusable
    
    * pull
    
    * refractor view stamp
    
    * delete stamp implemented
    
    * add stamp
    
    * edit implemented
    
    * reward drawer and pagination, infinite scroll
    
    * removed testing screen for pr review
    
    * padding on text input
    
    * removed stamp enabled
    
    * added bagde status on stamp cards
    
    * layout conflicts

--- src/store/store-manager/stamp-store.ts
+++ src/store/store-manager/stamp-store.ts
@@ -1,4 +1,4 @@
-import { StampStore } from "@/type/store-manager/stamp";
+import { StampConfigureViewState, StampStore, StampViewState } from "@/type/store-manager/stamp";
 import { create } from "zustand";
 
 export const useStampStore = create<StampStore>((set) => ({
@@ -11,4 +11,30 @@ export const useStampStore = create<StampStore>((set) => ({
   setExpirationMode: (expiration_mode) => set({ expiration_mode }),
   setExpirationDays: (expiration_days) => set({ expiration_days }),
   reset: () => set({ total_stamps: 0, reward_id: "", expiration_mode: "none", expiration_days: 30 }),
+}));
+
+export const useStampViewStore = create<StampViewState>((set) => ({
+  activeTab: "draft",
+  stamps: [],
+  rewards: [],
+  loading: true,
+  endingId: null,
+  modal: null,
+  setActiveTab: (activeTab) => set({ activeTab }),
+  setStamps: (stamps) => set({ stamps }),
+  setRewards: (rewards) => set({ rewards }),
+  setLoading: (loading) => set({ loading }),
+  setEndingId: (endingId) => set({ endingId }),
+  setModal: (modal) => set({ modal }),
+}));
+
+export const useStampConfigureViewStore = create<StampConfigureViewState>((set) => ({
+  isSubmitting: false,
+  checkingActive: true,
+  rewards: [],
+  modal: null,
+  setIsSubmitting: (isSubmitting) => set({ isSubmitting }),
+  setCheckingActive: (checkingActive) => set({ checkingActive }),
+  setRewards: (rewards) => set({ rewards }),
+  setModal: (modal) => set({ modal }),
 }));
